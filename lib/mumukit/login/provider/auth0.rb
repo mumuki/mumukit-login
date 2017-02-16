9c30d94c1460a9cebb037abf0eede1fb2325b95d
Agustin Pina <aguspina87@gmail.com>
2017-02-16 11:55:19 -0300

    Add header script to lock

--- lib/mumukit/login/provider/auth0.rb
+++ lib/mumukit/login/provider/auth0.rb
@@ -10,6 +10,7 @@ class Mumukit::Login::Provider::Auth0 < Mumukit::Login::Provider::Base
   def request_authentication!(controller, login_settings)
     settings = lock_settings(controller, login_settings, {closable: false})
     controller.render_html! <<HTML
+ #{header_html}
  <script type="text/javascript">
       new Auth0Lock('#{auth0_config.client_id}', '#{auth0_config.domain}').show(#{settings});
   </script>
