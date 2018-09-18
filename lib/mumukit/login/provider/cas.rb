5c9253065b8aa6fd28c45dd32f8e7532e3e7482b
Rodrigo Alfonso <rodri042@gmail.com>
2018-09-18 17:21:22 -0300

    Adding phpCAS compatibility

--- lib/mumukit/login/provider/cas.rb
+++ lib/mumukit/login/provider/cas.rb
@@ -13,3 +13,30 @@ class Mumukit::Login::Provider::Cas < Mumukit::Login::Provider::Base
     Mumukit::Login.config.cas
   end
 end
+
+# ---
+
+module OmniAuth
+  module Strategies
+    class CAS
+      def callback_phase
+        # Monkey-patching to support phpCAS implementation
+        # where the first time the 'ticket' param is not sent.
+        if !on_sso_path? && !request.params['ticket']
+          return request_phase
+        end
+
+        # Original method
+        if on_sso_path?
+          single_sign_out_phase
+        else
+          @ticket = request.params['ticket']
+          fail!(:no_ticket, MissingCASTicket.new('No CAS Ticket')) unless @ticket
+          fetch_raw_info(@ticket)
+          return fail!(:invalid_ticket, InvalidCASTicket.new('Invalid CAS Ticket')) if raw_info.empty?
+          super
+        end
+      end
+    end
+  end
+end
