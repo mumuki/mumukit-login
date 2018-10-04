f78903bd1974e8ad324f3c0a70812806b0cba114
Franco Bulgarelli <franco@mumuki.org>
2018-10-04 11:11:55 -0300

    Removing cas-specific-provider hacks

--- lib/mumukit/login/provider/cas.rb
+++ lib/mumukit/login/provider/cas.rb
@@ -10,24 +10,6 @@ class Mumukit::Login::Provider::Cas < Mumukit::Login::Provider::Base
   end
 end
 
-# Monkey-patching to support phpCAS implementation
-# where the first time the 'ticket' param is not sent.
-module OmniAuth
-  module Strategies
-    class CAS
-      alias_method :__callback_phase__, :callback_phase
-
-      def callback_phase
-        if !on_sso_path? && !request.params['ticket']
-          return request_phase
-        end
-
-        __callback_phase__
-      end
-    end
-  end
-end
-
 module OmniAuth
   module Strategies
     class CAS
