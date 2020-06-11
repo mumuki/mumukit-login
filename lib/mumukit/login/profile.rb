a124bfb860685e8b103a2bfb6facab1a63aef1ff
Julián Berbel Alt <julian@mumuki.org>
2020-06-11 19:20:20 -0300

    Compact identity hash to prevent unsetting profile info if null

--- lib/mumukit/login/profile.rb
+++ lib/mumukit/login/profile.rb
@@ -1,12 +1,18 @@
 module Mumukit::Login::Profile
   def self.from_omniauth(omniauth)
-    struct provider: omniauth.provider,
-           first_name: omniauth.info.first_name,
-           last_name: omniauth.info.last_name,
-           social_id: omniauth.uid,
-           email: omniauth.info.email,
-           uid: generate_uid(omniauth),
-           image_url: omniauth.info.image
+    struct identity_hash(omniauth)
+  end
+
+  def self.identity_hash(omniauth)
+    {
+      provider: omniauth.provider,
+      first_name: omniauth.info.first_name,
+      last_name: omniauth.info.last_name,
+      social_id: omniauth.uid,
+      email: omniauth.info.email,
+      uid: generate_uid(omniauth),
+      image_url: omniauth.info.image
+    }.compact
   end
 
   def self.generate_uid(omniauth)
