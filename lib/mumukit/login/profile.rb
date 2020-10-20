07427d6ef18c496f3cdaac50c348d04211c9e16c
Julián Berbel Alt <julian@mumuki.org>
2020-10-20 13:58:40 -0300

    Ignore image_url if it's too long

--- lib/mumukit/login/profile.rb
+++ lib/mumukit/login/profile.rb
@@ -11,8 +11,12 @@ module Mumukit::Login::Profile
       social_id: omniauth.uid,
       email: omniauth.info.email,
       uid: generate_uid(omniauth),
-      image_url: omniauth.info.image
+      image_url: image_url(omniauth.info.image)
     }.compact
+  end
+
+  def self.image_url(url)
+    url if url.try { size <= 255 }
   end
 
   def self.generate_uid(omniauth)
